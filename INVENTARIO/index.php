6bf139e64dad497e6f288887de4f308385ba4db4
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-08-22 13:08:47 +0200

    creando nuestra base para nuestro sistema, barra de navegacion bulma

--- INVENTARIO/index.php
+++ INVENTARIO/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Sistema de Inventario</title>
+   <?php 
+   include "./inc/head.php";
+   ?>
 </head>
 <body>
-    
+    <?php 
+        include "./inc/navbar.php";
+        include "./inc/script.php";
+    ?>
 </body>
 </html>
